221a9855063399df44103c964f402b90a518902f
nireo <eemil.lehtikangas1@gmail.com>
2022-12-05 22:21:46 +0200

    reworked manager to use serf

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/nireo/distsql
 go 1.18
 
 require (
-	github.com/casbin/casbin v1.9.1
 	github.com/hashicorp/raft v1.3.9
-	github.com/hashicorp/serf v0.9.8
+	github.com/hashicorp/raft-boltdb v0.0.0-20220329195025-15018e9b97e0
+	github.com/hashicorp/serf v0.10.1
 	github.com/mattn/go-sqlite3 v1.14.13
 	github.com/stretchr/testify v1.7.5
 	go.uber.org/zap v1.21.0
@@ -13,19 +13,14 @@ require (
 	google.golang.org/protobuf v1.28.0
 )
 
-require (
-	github.com/boltdb/bolt v1.3.1 // indirect
-	github.com/hashicorp/raft-boltdb v0.0.0-20220329195025-15018e9b97e0 // indirect
-)
+require github.com/boltdb/bolt v1.3.1 // indirect
 
 require (
-	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
 	github.com/armon/go-metrics v0.4.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/btree v1.1.2 // indirect
-	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-hclog v1.2.1 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
@@ -33,7 +28,7 @@ require (
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/hashicorp/memberlist v0.3.1 // indirect
+	github.com/hashicorp/memberlist v0.5.0 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/miekg/dns v1.1.50 // indirect
@@ -43,7 +38,7 @@ require (
 	go.uber.org/multierr v1.8.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
-	golang.org/x/sys v0.0.0-20220624220833-87e55d714810 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/tools v0.1.11 // indirect
 	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
---
+++ manager/manager.go
@@ -0,0 +1,150 @@
+package manager
+
+import (
+	"io"
+	"net"
+
+	"github.com/hashicorp/serf/serf"
+	"go.uber.org/zap"
+)
+
+// Config has all the configurable fields for Registry.
+type Config struct {
+	NodeName       string
+	BindAddr       string
+	Tags           map[string]string
+	StartJoinAddrs []string
+}
+
+// Handler represents a interface to a internal handler that also needs information about
+// any possible joins or leaves. In actual use the store.Store is passed here because Raft
+// needs to know of any possible changes to the cluster.
+type Handler interface {
+	Join(id, addr string) error
+	Leave(id string) error
+}
+
+// Manager handles service discovery and keeping track of the nodes currently in the cluster.
+type Manager struct {
+	Config
+	handler Handler
+	serf    *serf.Serf
+	events  chan serf.Event
+	logger  *zap.Logger
+}
+
+// New creates a registry instance and sets up serf for service discovery. This function
+// starts up the whole registry functionality by running an event handler, connection to
+// existing nodes and managing possible joins/leaves.
+func New(handler Handler, config Config) (*Manager, error) {
+	r := &Manager{
+		Config:  config,
+		handler: handler,
+		logger:  zap.L().Named("registry"),
+	}
+
+	if err := r.setupSerf(); err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+// setupSerf sets up a listener, configuration and event channel for serf to use. It
+// also attemps to connect to any nodes listed in StartJoinAddrs. It also starts the
+// eventHandler in a goroutine to handle any incoming serf events.
+func (r *Manager) setupSerf() error {
+	addr, err := net.ResolveTCPAddr("tcp", r.BindAddr)
+	if err != nil {
+		return err
+	}
+
+	config := serf.DefaultConfig()
+	config.Init() // allocate subdata structures
+
+	config.LogOutput = io.Discard
+	config.MemberlistConfig.BindAddr = addr.IP.String()
+	config.MemberlistConfig.BindPort = addr.Port
+
+	r.events = make(chan serf.Event)
+	config.EventCh = r.events
+	config.Tags = r.Tags
+	config.NodeName = r.NodeName
+
+	r.serf, err = serf.Create(config)
+	if err != nil {
+		return err
+	}
+
+	go r.eventHandler()
+	if r.StartJoinAddrs != nil {
+		if _, err := r.serf.Join(r.StartJoinAddrs, true); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// eventHandler is run concurrently and it listens for items in the event channel.
+// Then events that arrive in the event channel are handled. The only events that
+// matter here are serf.EventMemberJoin and serv.EventMemberLeave
+func (r *Manager) eventHandler() {
+	for e := range r.events {
+		switch e.EventType() {
+		case serf.EventMemberJoin:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if r.isLocal(member) {
+					continue
+				}
+				r.handleJoin(member)
+			}
+		case serf.EventMemberLeave:
+			for _, member := range e.(serf.MemberEvent).Members {
+				if r.isLocal(member) {
+					continue
+				}
+				r.handleLeave(member)
+			}
+		}
+	}
+}
+
+// handleJoin sends information to the internal handler to add given node to the cluster.
+func (r *Manager) handleJoin(member serf.Member) {
+	if err := r.handler.Join(member.Name, member.Tags["rpc_addr"]); err != nil {
+		r.logError(err, "failed to join", member)
+	}
+}
+
+// handleLeave sends information to the internal handler to remove given node.
+func (r *Manager) handleLeave(member serf.Member) {
+	if err := r.handler.Leave(member.Name); err != nil {
+		r.logError(err, "failed to leave", member)
+	}
+}
+
+// isLocal checks wheter the given Serf member is the local member
+// by checking the members' names
+func (r *Manager) isLocal(member serf.Member) bool {
+	return r.serf.LocalMember().Name == member.Name
+}
+
+// Members returns a point-in-time snapshot of the cluster's members.
+func (r *Manager) Members() []serf.Member {
+	return r.serf.Members()
+}
+
+// Leave tells this member to leave the cluster.
+func (r *Manager) Leave() error {
+	return r.serf.Leave()
+}
+
+func (r *Manager) logError(err error, msg string, member serf.Member) {
+	r.logger.Error(
+		msg,
+		zap.Error(err),
+		zap.String("name", member.Name),
+		zap.String("rcp_addr", member.Tags["rcp_addr"]),
+	)
+}
